40906207f9895fa029b40bb9a8b7c6128166652d
ramos tito <yramost@unprg.edu.pe>
2020-01-10 03:26:26 -0500

    se agrego el 3 ejercicio de menu

---
+++ sanchesolivos/ejercicio3.py
@@ -0,0 +1,36 @@
+from sanchez import libreria
+
+
+def precioguitarra():
+    a = libreria.pedir_numero("ingrese el precio de la guitarra: ", 0, 500)
+    print("el precio es: " + str(a) + " " + "$")
+    contenido = str(a) + "-" + "\n"
+    libreria.guardar_datos("info.txt", contenido, "a")
+    print("se han guardado los datos")
+
+def preciobateria():
+    b = libreria.pedir_numero("ingrese el precio de la bateria: ", 0, 500)
+    print("el precio de la bateria es: " + str(b) + " " + "$")
+    contenido = str(b) + "\n"
+    libreria.guardar_datos("info.txt", contenido, "a")
+    print("se han guardado los datos")
+
+opc = 0
+final = 3
+while (opc != final):
+    print("=====================================")
+    print("1. GUITARRA: el precio de la guitarra")
+    print("2. BATERIA: el precio de la bateria")
+    print("3. SALIR: fin del programa")
+    print("=====================================")
+
+    opc = libreria.pedir_numero("ingrese la opcion : ", 1, 3)
+
+    if (opc == 1):
+        precioguitarra()
+
+    if (opc == 2):
+        preciobateria()
+
+# fin_menu
+print("Fin Del Programa")

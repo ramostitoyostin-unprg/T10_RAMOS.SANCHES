d3ea9a9be65541c41c3ce8df5334ceb0462fcdbc
ramos tito <yramost@unprg.edu.pe>
2020-01-10 03:24:07 -0500

    se agrego el 1 ejercicio de menu

---
+++ sanchesolivos/ejercicio1.py
@@ -0,0 +1,37 @@
+from sanchez import libreria
+
+
+def opcionSumar():
+    x = libreria.pedir_numero("INGRESE EL PRIMER NUMERO: ", 0, 300)
+    y = libreria.pedir_numero("INGRESE EL SEGUNDO NUMERO: ", 0, 300)
+    print("la suma es: ", x + y)
+    contenido = str(x) + "-" + str(y) + "\n"
+    libreria.guardar_datos("info.txt", contenido, "a")
+    print("los datos han sido guardados")
+
+def opcionRestar():
+    x = libreria.pedir_numero("INGRESE EL PRIMER NUMERO: ", 0, 300)
+    y = libreria.pedir_numero("INGRESE EL SEGUNDO NUMERO: ", 0, 300)
+    print("la resta es: ", x - y)
+    contenido = str(x) + "-" + str(y) + "\n"
+    libreria.guardar_datos("info.txt", contenido, "a")
+    print("los datos han sido guardados")
+
+opc=0
+fin = 3
+while(opc != fin):
+    print("########## ADICION Y SUTRACION ###########")
+    print("1. SUMAR: suma 2 numeros")
+    print("2. RESTAR: resta 2 numeros")
+    print("3. SALIR: fin del programa")
+    print("##########################################")
+
+    opc = libreria.pedir_numero("ingrese su opcion: ", 1, 3)
+
+    if ( opc == 1):
+        opcionSumar()
+    if (opc == 2):
+        opcionRestar()
+
+# fin_menu
+print("FIN DEL PROGRAMA")
